c84043170cf334de14b9f21ee58fd704cd82209a
sabeeh6 <sabeehrajput6@gmail.com>
2026-04-15 18:47:30 +0500

    fix the conditional error

--- controllers/auth/authService.js
+++ controllers/auth/authService.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv"
 dotenv.config()
 
 if(!process.env.JWT_SECRET_123){
-    throw new error("Key is missing")
+    throw new Error("JWT_SECRET_123 is missing in Environment Variables")
 }
 // console.log("key" , process.env.JWT_SECRET_123);
 
